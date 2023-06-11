4185d0d69ab03972598a247cd810fc7885687717
Ryan <Rycorlin@gmail.com>
2023-06-10 20:51:59 -0400

    Updated to use monocart-reporter + generate lcov file

--- WebApplication3/Playwright/test.spec.js
+++ WebApplication3/Playwright/test.spec.js
@@ -1,5 +1,11 @@
 ﻿const { test, expect } = require('@playwright/test');
-const v8toIstanbul = require('v8-to-istanbul');
+import { attachCoverageReport } from 'monocart-reporter';
+
+//const v8toIstanbul = require('v8-to-istanbul');
+
+test.describe.configure({
+    mode: 'serial'
+});
 
 test('My Playwright Test', async ({ page }) => {
     // Your Playwright test code here
@@ -11,12 +17,20 @@ test('My Playwright Test', async ({ page }) => {
     expect(pageTitle).toBe('Home Page - WebApplication3');
 
     const coverage = await page.coverage.stopJSCoverage();
-    for (const entry of coverage) {
-        const converter = v8toIstanbul('', 0, { source: entry.source });
-        await converter.load();
-        converter.applyCoverage(entry.functions);
-        console.log(JSON.stringify(converter.toIstanbul()));
-    }
 
-    
+    // v8-to-stanbul attempt
+    //for (const entry of coverage) {
+    //    const converter = v8toIstanbul('', 0, { source: entry.source });
+    //    await converter.load();
+    //    converter.applyCoverage(entry.functions);
+    //    console.log(JSON.stringify(converter.toIstanbul()));
+    //}
+
+    const coverageData = await page.evaluate(() => window.__coverage__);
+
+    const report = await attachCoverageReport(coverageData, test.info(), {
+        lcov: true
+    });
+    //console.log(report.summary);
+
 });
--- WebApplication3/playwright.config.js
+++ WebApplication3/playwright.config.js
@@ -21,7 +21,13 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+    reporter: [
+        ['list'],
+        ['monocart-reporter', {
+            name: "My Test Report",
+            outputFile: './test-results/report.html'
+        }]
+    ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -29,7 +35,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-  },
+    },
 
   /* Configure projects for major browsers */
   projects: [
