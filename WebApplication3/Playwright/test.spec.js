bd791b5473a3d9e2773017be52a6ab2b97dc3c2b
Ryan <Rycorlin@gmail.com>
2023-06-10 20:30:30 -0400

    Add project files.

---
+++ WebApplication3/Playwright/test.spec.js
@@ -0,0 +1,22 @@
+﻿const { test, expect } = require('@playwright/test');
+const v8toIstanbul = require('v8-to-istanbul');
+
+test('My Playwright Test', async ({ page }) => {
+    // Your Playwright test code here
+
+    await page.coverage.startJSCoverage();
+
+    await page.goto('https://localhost:44348/');
+    const pageTitle = await page.title();
+    expect(pageTitle).toBe('Home Page - WebApplication3');
+
+    const coverage = await page.coverage.stopJSCoverage();
+    for (const entry of coverage) {
+        const converter = v8toIstanbul('', 0, { source: entry.source });
+        await converter.load();
+        converter.applyCoverage(entry.functions);
+        console.log(JSON.stringify(converter.toIstanbul()));
+    }
+
+    await browser.close();
+});
---
+++ WebApplication3/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+function subtractNumbers(a, b) {
+    return a - b;
+}
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+function divideNumbers(a, b) {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero!");
+    }
+    return a / b;
+}
+
+
+
+console.log(addNumbers(2, 3));
+console.log(subtractNumbers(5, 2));
+console.log(multiplyNumbers(4, 6));
+console.log(divideNumbers(10, 2));
+console.log(divideNumbers(8, 0)); // This will throw an error
